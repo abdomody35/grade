ace6b166e57a1220584e8c3b23f0a075e8ad15cd
abdomody35 <abdomody35@gmail.com>
2024-12-12 20:13:19 +0200

    function that ensures writing

---
+++ write.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int write_all(int fd, const char *buf)
+{
+    int count = strlen(buf), written;
+
+    while ((written = write(fd, buf, count)) > 0)
+    {
+        count -= written;
+        buf += written;
+    }
+
+    if (written == -1)
+    {
+        perror("write failed");
+        return -1;
+    }
+
+    return 0;
+}
